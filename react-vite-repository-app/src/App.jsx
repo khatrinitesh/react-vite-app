f6409d4a9616d8f69b132a9e9487d001ebce6921
niteshkhatri88 <nitesh.k@adsmn.in>
2024-04-28 14:21:58 +0530

    udpated merged

--- react-vite-repository-app/src/App.jsx
+++ react-vite-repository-app/src/App.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
 import CustomApp from './repository/modalboxes/1/app';
-=======
-import CustomApp from './repository/loadmore/1/Custom';
->>>>>>> 2d136274920589166c7d6fe42766827cf32431d3
 
 function App() {
 
